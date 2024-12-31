770876992da2d2a3026e28ed8f6efb2179b7be94
Huang <iozenhuang@hotmail.com>
2024-12-31 10:19:23 -0300

    Criando variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroInteiro = 5;
+        double numeroDecimal = 5.3;
+        String palavra = "Huang";
+        final double CONSTANTE = 3.14;
+
+        System.out.println("O numero inteiro é: "+ numeroInteiro);
+        System.out.println("O numero decimal é: "+ numeroDecimal);
+        System.out.println("A palavra é: "+ palavra);
+        System.out.println("O valor da constante é: "+ CONSTANTE);
     }
 }
